7322ceea52d43dd831c26304ad27b25f40a82787
Maria Bychkova <mar.byckova@gmail.com>
2022-03-19 14:54:08 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        int currentMoney = 100;
+        int newMoney = 2445;
+        int moneyAmount = 0;
+        int bonusAmount = 0;
+
+        if (newMoney >= 1000) {
+            bonusAmount = newMoney / 100;
+        }
+        moneyAmount = currentMoney + newMoney + bonusAmount;
+        System.out.println("Ваш баланс: " + moneyAmount + " рублей, из них бонусов - " + bonusAmount + " рублей");
+    }
+}
